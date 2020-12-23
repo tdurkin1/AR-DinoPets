a95ccdc53d52985aff8e6ae4733cee69c02c9cc7
Tomas Durkin <tdurkin1@gmail.com>
2020-12-23 03:34:11 -0800

    Added Dinosaur Selection

---
+++ AR DinoPets/Assets/Scripts/DinoManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class DinoManager : MonoBehaviour
+{
+    [SerializeField] MakeAppearOnPlane dinoPlacer;
+    [SerializeField] Transform triceratopsPen;
+    [SerializeField] Transform velociraptorPen;
+
+    
+
+    public void PlaceTriceratops()
+    {
+        dinoPlacer.content = triceratopsPen;
+    }
+
+    public void PlaceVelociraptor()
+    {
+        dinoPlacer.content = triceratopsPen;
+    }
+}
+
+
