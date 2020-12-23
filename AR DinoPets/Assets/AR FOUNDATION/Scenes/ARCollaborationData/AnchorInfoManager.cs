be3d8437fd1b5d0ae17ae5b2ea2d1932035bf61b
Tomas Durkin <tdurkin1@gmail.com>
2020-12-23 00:34:56 -0800

    Import existing project

---
+++ AR DinoPets/Assets/AR FOUNDATION/Scenes/ARCollaborationData/AnchorInfoManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+/// <summary>
+/// Displays information about each anchor including
+/// whether or not the anchor is local or remote.
+/// The anchor prefab is assumed to include a GameObject
+/// which can be colored to indicate which session created it.
+/// </summary>
+[RequireComponent(typeof(ARSessionOrigin))]
+[RequireComponent(typeof(ARAnchorManager))]
+public class AnchorInfoManager : MonoBehaviour
+{
+    [SerializeField]
+    ARSession m_Session;
+
+    public ARSession session
+    {
+        get { return m_Session; }
+        set { m_Session = value; }
+    }
+
+    void OnEnable()
+    {
+        GetComponent<ARAnchorManager>().anchorsChanged += OnAnchorsChanged;
+    }
+
+    void OnDisable()
+    {
+        GetComponent<ARAnchorManager>().anchorsChanged -= OnAnchorsChanged;
+    }
+
+    void OnAnchorsChanged(ARAnchorsChangedEventArgs eventArgs)
+    {
+        foreach (var anchor in eventArgs.added)
+        {
+            UpdateAnchor(anchor);
+        }
+
+        foreach (var anchor in eventArgs.updated)
+        {
+            UpdateAnchor(anchor);
+        }
+    }
+
+    unsafe struct byte128
+    {
+        public fixed byte data[16];
+    }
+
+    void UpdateAnchor(ARAnchor anchor)
+    {
+        var canvas = anchor.GetComponentInChildren<Canvas>();
+        if (canvas == null)
+            return;
+
+        canvas.worldCamera = GetComponent<ARSessionOrigin>().camera;
+
+        var text = canvas.GetComponentInChildren<Text>();
+        if (text == null)
+            return;
+
+        var sessionId = anchor.sessionId;
+        if (sessionId.Equals(session.subsystem.sessionId))
+        {
+            text.text = $"Local";
+        }
+        else
+        {
+            text.text = $"Remote";
+        }
+
+        var cube = anchor.transform.Find("Scale/SessionId Indicator");
+        if (cube != null)
+        {
+            var renderer = cube.GetComponent<Renderer>();
+            {
+                // Generate a color from the sessionId
+                Color color;
+                unsafe
+                {
+                    var bytes = *(byte128*)&sessionId;
+                    color = new Color(
+                        bytes.data[0] / 255f,
+                        bytes.data[4] / 255f,
+                        bytes.data[8] / 255f,
+                        bytes.data[12] / 255f);
+                }
+                renderer.material.color = color;
+            }
+        }
+    }
+}
---
+++ AR DinoPets/Assets/Scripts/ShowNavmesh.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ShowNavmesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void ShowMesh()
+    {
+        NavMeshTriangulation meshData = NavMesh.CalculateTriangulation();
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = meshData.vertices;
+        mesh.triangles = meshData.indices;
+
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShowMesh();
+    }
+}
